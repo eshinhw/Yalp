cd066adbcc4aaa405e3016de027c93cf78be8b4d
Eddie Shin <eshinhw@gmail.com>
2022-04-07 15:20:10 -0400

    setting up API connection

--- app/src/main/java/com/eddieshin/yelpclone/MainActivity.kt
+++ app/src/main/java/com/eddieshin/yelpclone/MainActivity.kt
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val BASE_URL = "https://api.yelp.com/v3/"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
